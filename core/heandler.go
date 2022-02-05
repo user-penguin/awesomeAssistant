4d67b372ca76137e37fb7060d14dd9bc5dbd12b0
user-penguin <kobzev96@gmail.com>
2022-02-06 06:37:28 +0700

    add new command "/system"

--- core/heandler.go
+++ core/heandler.go
@@ -1,16 +1,26 @@
 package core
 
 import (
+	"awesomeAssistant/prometheus"
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 )
 
 func HandleMessage(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
 	chatId := update.Message.Chat.ID
-	println(update.Message.Text)
-	_, err := bot.Send(tgbotapi.NewMessage(chatId, "Hello"))
-	if err != nil {
-		log.Println(err)
+	switch update.Message.Text {
+	case "/start":
+		_, err := bot.Send(tgbotapi.NewMessage(chatId, "Добро пожаловать к умному ассистенту!"))
+		if err != nil {
+			log.Println(err)
+		}
+	case "/system":
+		a, b := prometheus.FreeRam()
+		time := fmt.Sprint(a)
+		_, err := bot.Send(tgbotapi.NewMessage(chatId, "time = "+time+", value = "+b+"%"))
+		if err != nil {
+			log.Println(err)
+		}
 	}
-
 }
